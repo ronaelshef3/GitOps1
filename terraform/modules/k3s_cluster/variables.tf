8888075393e3406398d5a9b8a7207dd8a37b6e28
Aharon <you@example.com>
2025-12-29 08:17:38 +0200

    add argo files

--- terraform/modules/k3s_cluster/variables.tf
+++ terraform/modules/k3s_cluster/variables.tf
@@ -1,7 +1,10 @@
-variable "aws_region" { type = string }
+variable "aws_region" {
+  type = string
+}
 variable "instance_type" { type = string }
 variable "env_name" { type = string }
 variable "k3s_token" {
   type      = string
   sensitive = true
 }
+variable "key_name" {type = string}
