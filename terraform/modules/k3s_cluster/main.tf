6649490cae4d5ef25cedb816f1b4d2f21f283d58
Aharon <you@example.com>
2026-01-01 23:05:52 +0200

    define key

--- terraform/modules/k3s_cluster/main.tf
+++ terraform/modules/k3s_cluster/main.tf
@@ -27,8 +27,14 @@ resource "aws_instance" "k3s_node" {
       mkdir -p ../../local_backups/logs
       scp -o StrictHostKeyChecking=no -i ${var.private_key_path} ubuntu@${self.public_ip}:/var/log/syslog ../../local_backups/logs/syslog.log
     EOT
-  tags = { Name = "K3s-${var.env_name}" }
+
+  }
+  tags = {
+      Name = "K3s-${var.env_name}"
+      KeyPath = var.private_key_path
+   }
 }
+
 
 resource "aws_security_group" "k3s_sg" {
   name = "k3s-sg-${var.env_name}"
